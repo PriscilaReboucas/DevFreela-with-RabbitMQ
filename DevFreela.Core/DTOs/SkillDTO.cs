637e532a12e7f69b06c14f0a353922f434b7cf8d
Priscila Rebouças dos Santos <priscilaresantos@hotmail.com>
2023-11-29 01:30:35 -0300

    teste commit

--- DevFreela.Core/DTOs/SkillDTO.cs
+++ DevFreela.Core/DTOs/SkillDTO.cs
@@ -7,6 +7,6 @@ namespace DevFreela.Core.DTOs
     public class SkillDTO
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; }//
     }
 }
